1160aafe2a92c4369dbd1f7099132fc52b3efab9
zouhaofeng <zhfthss@126.com>
2014-09-28 16:48:16 +0800

    more tests

--- test.js
+++ test.js
@@ -29,14 +29,27 @@ function tRandom(dense){
 	return result;
 }
 
-// test: evolve one generation
+// test: evolve one step with random distribution
 function tCount(dense){
-	var dx = [-1, -1, -1, 0, 0, 1, 1, 1];
-	var dy = [-1, 0, 1, -1, 1, -1, 0, 1];
 	var game = new Game();
 	game.init();
 	game.density = dense;
 	game.random();
+	return check(game);	
+}
+
+function tSpecial(){
+	var game = new Game();
+	game.init();
+	game.map[0][0] = game.map[game.width-1][0] = game.map[0][game.height-1] = game.map[game.width-1][game.height-1] = 1;
+	game.count();
+	var result = game.map[0][0] + game.map[game.width-1][0] + game.map[0][game.height-1] + game.map[game.width-1][game.height-1];
+	return result;
+}
+
+function check(game){
+	var dx = [-1, -1, -1, 0, 0, 1, 1, 1];
+	var dy = [-1, 0, 1, -1, 1, -1, 0, 1];
 	var prev = new Array();
 	for (var i = 0; i < game.width; i++)
 		prev[i] = new Array();
@@ -75,6 +88,12 @@ function tCount(dense){
 	return true;
 }
 
+
+/*                */
+/* test functions */
+/*                */
+module("Tests for initial part:");
+
 test("initialize", function(){
 	ok(tInit(), 'Successfully initialized');
 })
@@ -87,7 +106,9 @@ test("randomize", function(){
 	equal(tRandom(1), 100, 'Generate cells with density 100%');
 })
 
-test("evolve", function (){
+module("Tests for logical part:");
+
+test("test:evolve one step(with different density)", function (){
 	ok(tCount(0), 'Evolve correctly');
 	ok(tCount(0.1), 'Evolve correctly');
 	ok(tCount(0.2), 'Evolve correctly');
@@ -99,4 +120,27 @@ test("evolve", function (){
 	ok(tCount(0.8), 'Evolve correctly');
 	ok(tCount(0.9), 'Evolve correctly');
 	ok(tCount(1), 'Evolve correctly');
+})
+
+test("test:evolve multiple steps", function(){
+	var game = new Game();
+	game.init();
+	game.density = 0.5;
+	game.random();
+	for (i = 0; i < 100; i++)
+	{
+		game.count();
+		equal(check(game), true, "evolve step"+(i+1));
+	}
+})
+
+test("special test sample", function(){	
+	equal(tSpecial(), 4, 'Correct');
+})
+
+asyncTest('asynchronous test', function() { 
+	setTimeout(function() {
+	    ok(tCount(0.5), 'Correct');
+	    start();
+	}, 100)
 })
